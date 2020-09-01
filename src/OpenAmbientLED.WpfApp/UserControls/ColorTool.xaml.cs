aea2d1acd1803784b7b9845684ec78a8fe8c6ab9
Welbert Gomides <welbert.gomides@gmail.com>
2020-09-01 18:12:08 -0300

    Removing ColorTool ViewModel

--- src/OpenAmbientLED.WpfApp/UserControls/ColorTool.xaml.cs
+++ src/OpenAmbientLED.WpfApp/UserControls/ColorTool.xaml.cs
@@ -1,23 +1,78 @@
-﻿using OpenAmbientLED.WpfApp.Models;
+﻿using MaterialDesignColors;
+using MaterialDesignColors.Recommended;
+using OpenAmbientLED.WpfApp.Commands;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace OpenAmbientLED.WpfApp.UserControls
 {
-    /// <summary>
-    /// Interaction logic for ColorTool.xaml
-    /// </summary>
-    public partial class ColorTool : UserControl
+    public delegate void ColorChangedEventHandler(Color color);
+
+    public partial class ColorTool : UserControl, INotifyPropertyChanged
     {
         public ColorTool()
         {
+            ChangeHueCommand = new AnotherCommandImplementation(ChangeHue);
+
+            DataContext = this;
             InitializeComponent();
-            DataContext = new ColorToolViewModel(OnColorChanged);
         }
 
-        public event ColorChangedEventHandler ColorChanged;
+        private Color? _selectedColor;
+        public Color? SelectedColor
+        {
+            get => _selectedColor;
+            set
+            {
+                if (_selectedColor != value)
+                {
+                    _selectedColor = value;
+                    OnPropertyChanged();
 
-        public void OnColorChanged(Color color)
+                    if (value is Color color)
+                        OnColorChanged(color);
+                }
+            }
+        }
+
+        public ICommand ChangeHueCommand { get; }
+
+        private void ChangeHue(object obj)
+            => SelectedColor = (Color)obj;
+
+        public IEnumerable<ISwatch> Swatches { get; } = new ISwatch[]
+        {
+                new RedSwatch(),
+                new PinkSwatch(),
+                new PurpleSwatch(),
+                new DeepPurpleSwatch(),
+                new IndigoSwatch(),
+                new BlueSwatch(),
+                new LightBlueSwatch(),
+                new CyanSwatch(),
+                new TealSwatch(),
+                new GreenSwatch(),
+                new LightGreenSwatch(),
+                new LimeSwatch(),
+                new YellowSwatch(),
+                new AmberSwatch(),
+                new OrangeSwatch(),
+                new DeepOrangeSwatch(),
+                new BrownSwatch(),
+                new GreySwatch(),
+                new BlueGreySwatch()
+        };
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public event ColorChangedEventHandler ColorChanged;
+        protected virtual void OnColorChanged(Color color)
             => this.ColorChanged.Invoke(color);
     }
 }
